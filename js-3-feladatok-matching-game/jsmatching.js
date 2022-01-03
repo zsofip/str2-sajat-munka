31ae34d6905647aba5e2c5587f311c64397cea63
zsofip <91950899+zsofip@users.noreply.github.com>
2022-01-03 02:26:36 +0100

    done

--- js-3-feladatok-matching-game/jsmatching.js
+++ js-3-feladatok-matching-game/jsmatching.js
@@ -1,33 +1,56 @@
 'use strict';
 
-
-// Egy “Keresd a párját” játékot kell készítened
-// A játékban két sorban soronként 5, azaz összesen 10 darab kártya látható x
-// A játék kezdetekor a kártyák le vannak fordítva. A kártyák másik oldala egy egyszerű ábrát tartalmaz x
-// Az ábrák szabadon választható képek vagy akár ikonok is lehetnek a könnyebb felismerhetőség végett x
-// Minden ábrából összesen két darab van x
-// Amikor a játékos rákattint egy kártyára, az egy egyszerű animáció kíséretében megfordul, felfedve az ábrát x
-// Az első kártyára való kattintáskor elindul egy számláló, amely a játékidőt mutatja perc:másodperc formában
-const cards = document.querySelectorAll(".flip-card-inner");
+const cards = document.querySelectorAll(".flip-card");
+const cardsInner = document.querySelectorAll(".flip-card-inner");
 
 let hasFlippedCard = false;
 let lockCards = false;
 let firstCard;
 let secondCard;
+let matchedCards = [];
 
+const timer = document.querySelector("#timer");
+let time;
+let min = 0;
+let sec = 0;
+let timeStart = false;
+
+function startTime() {
+  time = setInterval(function() {
+    sec++;
+    if (sec === 60) {
+    min++;
+    sec = 0;
+    } 
+    sec = checkTime(sec);
+    timer.innerHTML = `${min}:${sec}`;
+    }, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  
+  return i;
+}
 
 function flipCard() {
+  if (timeStart === false) {
+    timeStart = true; 
+    startTime();
+  };
   if (lockCards) {return lockCards};
+  if (this === firstCard) {return firstCard};
 
   this.classList.add("flip");
 
   if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
+    return firstCard;
   } else {
     secondCard = this;
+    lockCards = true;
     isMatch();
-  }  
+  } 
 };
 
 function isMatch() {
@@ -37,6 +60,7 @@ function isMatch() {
 function stopCards() {
   firstCard.removeEventListener("click", flipCard);
   secondCard.removeEventListener("click", flipCard);
+  matchedCards.push(firstCard, secondCard);
   newTurn();
 }
 
@@ -46,43 +70,58 @@ function turnBackCards() {
       firstCard.classList.remove("flip");
       secondCard.classList.remove("flip");
      newTurn();
-    }, 1400);
+    }, 1500);
 }
 
 function newTurn() {
   [hasFlippedCard, lockCards] = [false, false];
   [firstCard, secondCard] = [null, null];
+  if (matchedCards.length === 10) {
+    stopTime();
+  }
 }
 
-cards.forEach(card => card.addEventListener("click", flipCard));
+function stopTime() {
+  clearInterval(time);
+  setTimeout(resetGame, 5000);
+}
 
+function shuffleCards() {
+  cards.forEach(item => {
+    let randomOrder = Math.floor(Math.random() * 10);
+    item.style.order = randomOrder;
+  });
+}
 
+function resetGame() {
+  timeStart = false;
+  sec = 0;
+  min = 0;
+  timer.innerHTML = "00:00";
+  cardsInner.forEach(card => card.classList.remove("flip"));
+  matchedCards = [];
+  startGame();
+}
 
-// const startGame = () => {
-//   addClickListener();
-// };
+function startGame() {
+  cardsInner.forEach(card => card.addEventListener("click", flipCard));
+  shuffleCards();
+}
 
-// const addClickListener = () => {
-//   cards.addEventListener("click", startTime);
-// };
+startGame();
 
-// function startTime() {
-//     const time = new Date();
-//     let m = time.getMinutes();
-//     let s = time.getSeconds();
-//     m = checkTime(m);
-//     s = checkTime(s);
-//     document.getElementById('txt').innerHTML = `${m}:${s}`;
-//     setTimeout(startTime, 1000);
-//   }
-
-//   function checkTime(i) {
-//     if (i < 10) {i = "0" + i};  
-//     return i;
-//   }
+// Egy “Keresd a párját” játékot kell készítened
+// A játékban két sorban soronként 5, azaz összesen 10 darab kártya látható x
+// A játék kezdetekor a kártyák le vannak fordítva. A kártyák másik oldala egy egyszerű ábrát tartalmaz x
+// Az ábrák szabadon választható képek vagy akár ikonok is lehetnek a könnyebb felismerhetőség végett x
+// Minden ábrából összesen két darab van x
+// Amikor a játékos rákattint egy kártyára, az egy egyszerű animáció kíséretében megfordul, felfedve az ábrát x
+// Az első kártyára való kattintáskor elindul egy számláló, amely a játékidőt mutatja perc:másodperc formában x
 // Amennyiben egymás után két felfordított kártyán ugyanaz az ábra szerepel, azokat felfordítva kell hagyni a játék végéig, 
 // többet nem lehet rájuk kattintani x
 // Amennyiben a két ábra eltérő, mind a kettőt automatikusan vissza kell forgatni x
 // A játéknak akkor van vége, amikor az összes kártya képes oldala látszik x
-// A számláló a játék végén megáll
-// 5 másodperc múlva a számláló nullázódik, és az összes kártya visszafordul, kezdődhet egy új játék
+// A számláló a játék végén megáll x
+// 5 másodperc múlva a számláló nullázódik, x
+// és az összes kártya visszafordul, x
+// kezdődhet egy új játék x
