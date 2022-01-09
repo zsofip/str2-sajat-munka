9d1d26bfcec0239f83185576c9db47425096779c
zsofip <91950899+zsofip@users.noreply.github.com>
2022-01-09 22:39:38 +0100

    corrected version

--- bs-feladatok-sitebuilding/main.js
+++ bs-feladatok-sitebuilding/main.js
@@ -1,5 +1,42 @@
-$(function () {
+ $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
 
+  $(document).ready(function(){
+    $("a#Privacy").click(function(){
+        $("#PrivacyModal").modal('show');
+    });
+});
+
+  $(document).ready(function(){
+    $("a#Terms").click(function(){
+        $("#TermsModal").modal('show');
+    });
+});
   
+ $(document).ready(function(){
+    $("a#FAQ").click(function(){
+        $("#FAQModal").modal('show');
+    });
+});
+
+
+  const navBar = document.querySelector('.navbar');
+  const navBrand = document.querySelector('.navbar-brand');
+  const navLinks = document.querySelectorAll('.nav-link')
+  
+function changeNav() {
+  if (document.body.scrollTop !== 0 || document.documentElement.scrollTop !== 0) {
+    navBar.style.backgroundColor = 'white';
+    navBrand.style.color = 'grey';
+    navLinks.forEach(link => link.style.color = 'grey');
+  }  else {
+    navBar.style.backgroundColor = 'transparent';
+    navBrand.style.color = '#ffc107';
+    navLinks.forEach(link => link.style.color = '#ffc107');
+  }
+};
+
+changeNav();
+
+document.addEventListener('scroll', changeNav);
